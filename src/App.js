0aa4abd815e3db8702376a221fe0f37655c862e4
Ioana Postolache <postolac@adobe.com>
2021-08-26 16:19:39 +0300

    Added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
+
 import About from "./pages/about/About";
 import Users from "./pages/users/Users";
 import Home from "./pages/home/Home";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <div>
         <nav>
           <ul>
             <li>
-              <Link to="/pages/target-spa/tgt-spa/">Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/pages/target-spa/tgt-spa/about">About</Link>
+              <Link to="/about">About</Link>
             </li>
             <li>
-              <Link to="/pages/target-spa/tgt-spa/users">Users</Link>
+              <Link to="/users">Users</Link>
             </li>
           </ul>
         </nav>
@@ -25,17 +26,17 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/pages/target-spa/tgt-spa/about">
+          <Route path="/about">
             <About />
           </Route>
-          <Route path="/pages/target-spa/tgt-spa/users">
+          <Route path="/users">
             <Users />
           </Route>
-          <Route path="/pages/target-spa/tgt-spa/">
+          <Route path="/">
             <Home />
           </Route>
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
